20e0e1446ca96904376e6d779a4542532edc55ff
212222 <212222@1305-004>
2025-01-24 16:15:44 +0300

    Добавьте файлы проекта.

---
+++ Barcode11/BarcodeDbContext.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using static Barcode11.MainWindow;
+
+namespace Barcode11
+{
+    public class BarcodeDbContext : DbContext
+    {
+        public BarcodeDbContext() : base("name=BarcodeDBConnectionString") { }  
+        public DbSet<Product> Product { get; set; }
+    }
+}
